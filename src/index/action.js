d172fe9808799568171b12529a3d5bcd9d240a4e
jason <huanglin_jason@163.com>
2019-10-30 21:56:55 +0800

    init

--- src/index/action.js
+++ src/index/action.js
@@ -2,19 +2,125 @@ export const ACTION_SET_FORM = 'ACTION_SET_FORM'; // 起点站
 export const ACTION_TO = '上海'; // 终点站
 
 // 城市 弹窗 打开 关闭 状态
-export const ACTION_IS_CITYSELECTORVISIBLE = 'ACTION_IS_CITYSELECTORVISIBLE';
+export const ACTION_IS_CITY_SELECTOR_VISIBLE = 'ACTION_IS_CITY_SELECTOR_VISIBLE';
 
 // 起点站 是否 有值
-export const ACTION_CURRENTSELECTINGLEFTCITY = 'ACTION_CURRENTSELECTINGLEFTCITY';
+export const ACTION_CURRENT_SELECTING_LEFT_CITY = 'ACTION_CURRENT_SELECTING_LEFT_CITY';
 
 // 城市数据
-export const ACTION_CITYDATA = 'ACTION_CITYDATA';
+export const ACTION_CITY_DATA = 'ACTION_CITY_DATA';
 
 // 是否正在 加载 城市列表
-export const ACTION_ISLOADINGCITYDATA = 'ACTION_ISLOADINGCITYDATA';
+export const ACTION_IS_LOADING_CITY_DATA = 'ACTION_IS_LOADING_CITY_DATA';
 
 // 日期选择 弹窗 是否打开
-export const ACTION_ISDATESELECTORVISIBLE = 'ACTION_ISDATESELECTORVISIBLE';
+export const ACTION_IS_DATE_SELECTOR_VISIBLE = 'ACTION_IS_DATE_SELECTOR_VISIBLE';
 
 // 是否选择了 只看高铁动车
-export const ACTION_HIGHSPEED = 'ACTION_HIGHSPEED';
+export const ACTION_HIGH_SPEED = 'ACTION_HIGH_SPEED';
+
+// 日期选择
+export const ACTION_DEPART_DATE = 'ACTION_DEPART_DATE';
+
+
+
+export const setDepartDate = (date) => {
+  return (dispatch) => {
+    dispatch({
+      type: ACTION_DEPART_DATE,
+      payload: date,
+    })
+  }
+}
+
+export const setForm = (form) => {
+  return {
+    type: ACTION_SET_FORM,
+    payload: form,
+  }
+}
+
+export const setTo = (to) => {
+  return {
+    type: ACTION_TO,
+    payload: to,
+  }
+}
+
+export const setIsLoadingCityData = (isLoadingCityData) => {
+  return {
+    type: ACTION_IS_CITY_SELECTOR_VISIBLE,
+    payload: isLoadingCityData,
+  }
+}
+
+export const setCityData = (cityData) => {
+  return {
+    type: ACTION_CITY_DATA,
+    payload: cityData,
+  }
+}
+
+export const toggleHighSpeed = () => {
+  return (dispatch, getState) => {
+    const { highSpeed } = getState();
+    dispatch({
+      type: ACTION_HIGH_SPEED,
+      payload: !highSpeed,
+    });
+  }
+}
+
+export const showCityelector = (currentSelectingLeftCity) => {
+  return (dispatch) => {
+    dispatch({
+      type: ACTION_IS_DATE_SELECTOR_VISIBLE,
+      payload: true,
+    });
+
+    dispatch({
+      type: ACTION_CURRENT_SELECTING_LEFT_CITY,
+      paylaod: currentSelectingLeftCity,
+    })
+  }
+}
+
+export const hideCitySelector = () => {
+  return {
+    type: ACTION_IS_DATE_SELECTOR_VISIBLE,
+    payload: false,
+  }
+}
+
+export const setSelectedCity = (city) => {
+  return (dispatch, getState) => {
+    const { currentSelectingLeftCity } = getState();
+    if (currentSelectingLeftCity) {
+      dispatch(setForm(city));
+    } else {
+      dispatch(setTo(city))
+    }
+  }
+}
+
+export const showDateSelector = () => {
+  return {
+    type: ACTION_IS_DATE_SELECTOR_VISIBLE,
+    payload: true,
+  }
+}
+
+export const hideDateSelector = () => {
+  return {
+    type: ACTION_IS_DATE_SELECTOR_VISIBLE,
+    payload: false,
+  }
+}
+
+export const exchangeFromTo = () => {
+  return (dispatch, getState) => {
+    const { from, to } = getState;
+    dispatch(setForm(to));
+    dispatch(setTo(from));
+  }
+}
--- src/index/reducer.js
+++ src/index/reducer.js
@@ -1,3 +1,96 @@
-export default (state, action) => {
-  return state;
+import {
+  ACTION_SET_FORM,
+  ACTION_TO,
+  ACTION_IS_CITY_SELECTOR_VISIBLE,
+  ACTION_CURRENT_SELECTING_LEFT_CITY,
+  ACTION_CITY_DATA,
+  ACTION_IS_LOADING_CITY_DATA,
+  ACTION_IS_DATE_SELECTOR_VISIBLE,
+  ACTION_HIGH_SPEED,
+  ACTION_DEPART_DATE
+} from './action';
+
+
+export default {
+  from(state = "上海", action){ // 起点站
+    const { type, payload } = action;
+    switch (type) {
+      case ACTION_SET_FORM:
+        return payload;
+      default:
+    }
+    return state;
+  },
+  to(state = "北京", action){ // 终点站
+    const { type, payload } = action;
+    switch(type) {
+      case ACTION_TO:
+        return payload;
+      default:
+    }
+    return state;
+  },
+  departDate(state, action){ // 选择日期
+    const { type, payload } = action;
+    switch (type) {
+      case ACTION_DEPART_DATE:
+        return payload;
+      default:
+    }
+    return state;
+  },
+  isCitySelectorVisible(state  =  false, action){ // 城市选择浮层是否 打开
+    const { type, payload } = action;
+    switch(type) {
+      case ACTION_IS_CITY_SELECTOR_VISIBLE:
+        return payload;
+      default:
+    }
+    return state;
+  },
+  currentSelectingLeftCity(state = false, action) {// 起点站 是否选择
+    const { type, payload } = action;
+    switch (type) {
+      case ACTION_CURRENT_SELECTING_LEFT_CITY:
+        return payload;
+      default:
+    }
+    return state;
+  },
+  cityData(state = null, action) { // 城市数据
+    const { type, payload } = action;
+    switch(type) {
+      case ACTION_CITY_DATA:
+        return payload;
+      default:
+    }
+    return state;
+  },
+  isLoadingCityData(state = false, action) {// 是否正在加载 城市数据
+    const { type, payload } = action;
+    switch(type) {
+      case ACTION_IS_LOADING_CITY_DATA:
+        return payload;
+      default:
+    }
+    return state;
+  },
+  isDateSelectorVisible(state = false, action) {// 日期 选择浮层是否 打开
+    const { type, payload } = action;
+    switch(type) {
+      case ACTION_IS_DATE_SELECTOR_VISIBLE:
+        return payload;
+      default:
+    }
+    return state;
+  },
+  isHighSpeed(state = false, action) { // 是否选择 只看 高铁 动车
+    const { type, payload } = action;
+    switch(type) {
+      case ACTION_HIGH_SPEED:
+        return payload;
+      default:
+    }
+    return state;
+  },
 }
