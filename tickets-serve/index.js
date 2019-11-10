31ac97612a7dbf7db8709a14d2a2e105c86a56bf
iyb-huanglin <huanglin@iyunbao.com>
2019-11-10 21:49:23 +0800

    server

--- tickets-serve/index.js
+++ tickets-serve/index.js
@@ -1,21 +1,26 @@
 // 搭建 mock-serve 服务器
 
 const express = require('express');
+const path = require('path');
+const apiMocker = require('mocker-api');
+
 
 const app = express();
 
-app.get('/', (Request, Response) => {
-  Response.status(200);
-  Response.send('hello express');
-  Response.end();
-});
+// app.get('/', (Request, Response) => {
+//   Response.status(200);
+//   Response.send('hello express');
+//   Response.end();
+// });
 
-app.get('/rest', (Request, Response) => {
-  Response.json({
-    result: true,
-    message: 'hello express',
-  })
-});
+// app.get('/rest', (Request, Response) => {
+//   Response.json({
+//     result: true,
+//     message: 'hello express',
+//   })
+// });
+
+apiMocker(app, path.resolve('./mocker/mocker.js'))
 
 
 app.listen(8090);
